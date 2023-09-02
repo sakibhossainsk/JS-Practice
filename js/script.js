51db645a29ebfb0c81cdc2848e1ee483e21d1660
SakibHossain <136673238+sakibhossainsk@users.noreply.github.com>
2023-09-02 15:51:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//item 1 start
+//text change
+function textChange(){
+	document.getElementById('h2').innerHTML="Hello World.";
+}
+//item 1 end
+//item 2 start
+//text change
+function clickHere(){
+	document.getElementById('i_h2').innerHTML="Hello Bangladesh.";
+}
+//item 3 end
+//item 2 start
+//text change
+function clickMe(){
+	document.getElementById('i2_h2').innerHTML="Hello Dhaka.";
+}
+//item 3 end
+//item 4 start
+//text change
+function clickBtn(){
+	document.getElementById('i3_h2').innerHTML="Hello Rajshahi.";
+}
+//item 4 end
+//item 5 start
+//text change
+function clickTap(){
+	document.getElementById('i4_h2').innerHTML="Hello Naogaon.";
+}
+//item 5 end
+
+
+
+//item 6 start
+//text change
+function changeText(){
+	document.getElementById('i5_h2').innerHTML="Hello World.";
+}
+//item 6 end
+//item 7 start
+//text change
+function hereClick(){
+	document.getElementById('i6_h2').innerHTML="Hello Bangladesh.";
+}
+//item 7 end
+//item 8 start
+//text change
+function meClick(){
+	document.getElementById('i7_h2').innerHTML="Hello Dhaka.";
+}
+//item 8 end
+//item 9 start
+//text change
+function btnClick(){
+	document.getElementById('i8_h2').innerHTML="Hello Rajshahi.";
+}
+//item 9 end
+//item 10 start
+//text change
+function tapClick(){
+	document.getElementById('i9_h2').innerHTML="Hello Naogaon.";
+}
+//item 10 end
